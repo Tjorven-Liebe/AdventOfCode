400f94c5452a604467f4f52ee19d69ef4557c054
tjorven <tjorvenliebe@gmail.com>
2024-12-01 23:52:23 +0100

    fix: code cleanup

---
+++ src/main/java/de/tjorven/aoc24/one/PartB.java
@@ -0,0 +1,34 @@
+package de.tjorven.aoc24.one;
+
+import javafx.util.Pair;
+
+import java.io.IOException;
+
+public class PartB {
+
+    public static void main(String[] args) throws IOException {
+        String[] split = SolverUtil.getInput();
+        Pair<int[], int[]> arr = SolverUtil.getArr(split);
+        int[] first = arr.getKey();
+        int[] second = arr.getValue();
+
+        int sum = 0;
+        for (int index : first) {
+            int count = count(index, second);
+            sum += index * count;
+        }
+
+        System.out.println(sum);
+    }
+
+    private static int count(int index, int[] list) {
+        int count = 0;
+        for (int i : list) {
+            if (i == index) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
---
+++ src/main/java/de/tjorven/aoc24/one/SolverUtil.java
@@ -0,0 +1,38 @@
+package de.tjorven.aoc24.one;
+
+import javafx.util.Pair;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class SolverUtil {
+
+    private SolverUtil() {
+
+    }
+
+    public static String[] getInput() {
+        InputStream resourceAsStream = SolverUtil.class.getClassLoader().getResourceAsStream("one/input.txt");
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(resourceAsStream));
+        StringBuilder builder = new StringBuilder();
+        bufferedReader.lines().forEach(s -> builder.append(s).append("   "));
+
+        return builder.toString().split("   ");
+    }
+
+    public static Pair<int[], int[]> getArr(String[] input) {
+        int[] arrA = new int[input.length];
+        int[] arrB = new int[input.length];
+        int i = 0;
+        while (i < input.length-1) {
+            if (i++ % 2 == 0) {
+                arrA[i] = Integer.parseInt(input[i - 1]);
+            } else {
+                arrB[i] = Integer.parseInt(input[i - 1]);
+            }
+        }
+        return new Pair<>(arrA, arrB);
+    }
+
+}
